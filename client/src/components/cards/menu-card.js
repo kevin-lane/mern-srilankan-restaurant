3a30f11ad552f5961dbd87cbba0166a4de665c41
Kevin <kevinlane3@hotmail.com>
2025-05-17 21:14:07 +0200

    Testing localstorage

--- client/src/components/cards/menu-card.js
+++ client/src/components/cards/menu-card.js
@@ -5,6 +5,8 @@ import './menuCard.css';
 function MenuCard(props) {
   function addItem(){
     alert(props.name + " has been added to the cart ");
+    localStorage.setItem('cartName', props.name);
+    localStorage.setItem('cartPrice', props.price);
     axios.post(`${process.env.REACT_APP_BACKEND_URL}/addToCart`, { name: props.name, price: props.price })
       .then(res => {
         console.log(res);
@@ -12,6 +14,7 @@ function MenuCard(props) {
       })
       .catch(err => console.log(err));
   }
+
   return (
     <div id='menu-card' key={props.id}>
         <p id='menu-name'>{props.name}</p>
