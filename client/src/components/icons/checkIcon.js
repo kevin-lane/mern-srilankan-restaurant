7c6b1256a504cd12c155b3dc105d7fe4a7d2bc21
Kevin <kevinlane3@hotmail.com>
2025-05-20 21:27:54 +0200

    Correction on checkicon

--- client/src/components/icons/checkIcon.js
+++ client/src/components/icons/checkIcon.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function checkIcon() {
+function checkIcon() {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-check2-circle" viewBox="0 0 16 16">
   <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0"/>
@@ -8,3 +8,5 @@ export default function checkIcon() {
 </svg>
   )
 }
+
+export default checkIcon;
--- client/src/components/modals/OrderConfirmation.js
+++ client/src/components/modals/OrderConfirmation.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './orderConfirmation.css';
-import checkIcon from '../icons/checkIcon';
+import CheckIcon from '../icons/checkIcon';
 
 export default function() {
   return (
     <div id='order-confirmation'>
       <p>Thank you for your order. Please come to the restaurant and collect your order!</p>
-      <checkIcon />
+      <p><CheckIcon /></p>
       <button id='order-confirmation-button' onClick={() => window.location.reload()}>OK</button>
     </div>
   )
