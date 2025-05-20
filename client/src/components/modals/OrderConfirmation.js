efaade7e1f6a14c1cffff398b6760e766d7c4dad
Kevin <kevinlane3@hotmail.com>
2025-05-20 21:19:39 +0200

    implement check icon to the confirmation modal

--- client/src/components/modals/OrderConfirmation.js
+++ client/src/components/modals/OrderConfirmation.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './orderConfirmation.css';
+import checkIcon from '../icons/checkIcon';
 
 export default function() {
   return (
     <div id='order-confirmation'>
       <p>Thank you for your order. Please come to the restaurant and collect your order!</p>
+      <checkIcon />
       <button id='order-confirmation-button' onClick={() => window.location.reload()}>OK</button>
     </div>
   )
