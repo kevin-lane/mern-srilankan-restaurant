6c9ac2d4f34df895a4efe4606ea7dba756414ea6
Kevin <kevinlane3@hotmail.com>
2025-05-22 21:45:52 +0200

    key===id

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -30,7 +30,7 @@ function Cart() {
 
   let totalPrice = cartItems.reduce((prev, {price}) => prev + price, 0)
   let items = cartItems.map(i => i.name);
-  let sameID = cartItems.filter((c) => c);
+  let sameID = cartItems.filter(c => c.key === id);
   console.log(sameID);
 
 
