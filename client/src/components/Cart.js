0b90f28d48ee4f1e3870900bc65d01d0313f553d
Kevin <kevinlane3@hotmail.com>
2025-05-22 21:36:41 +0200

    Test id filtering

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -30,6 +30,9 @@ function Cart() {
 
   let totalPrice = cartItems.reduce((prev, {price}) => prev + price, 0)
   let items = cartItems.map(i => i.name);
+  let sameID = cartItems.filter((c) => c._id === id);
+  console.log(sameID);
+
 
   function submitOrder(){
     alert("Thank you for your order")
