fbdd689a211404116fa32752e20be7c64345ce07
Kevin <kevinlane3@hotmail.com>
2025-05-23 14:01:31 +0200

    check includes key

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -32,6 +32,7 @@ function Cart() {
   let items = cartItems.map(i => i.name);
   console.log(cartItems);
 
+  cartItems.includes(item => item.key);
   cartItems.forEach(cartItem => {
     console.log(cartItem.key);
 
