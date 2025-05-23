2e6f487beb89c0eed276d6bf338d52c1ab7994d3
Kevin <kevinlane3@hotmail.com>
2025-05-23 14:43:19 +0200

    prepare for amount of same item

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -97,6 +97,7 @@ function Cart() {
 
             return (
             <li className='cart-list-item'>
+              <div id='content-wrapper'><span id='cart-item-amount'>1x</span> </div>
               <div id='content-wrapper'><span id='cart-item-name'>{item.name}</span> </div>
               <div id='button-wrapper'><span id='cart-item-price'>{item.price}kr</span><button id='remove-btn' onClick={() => deleteItem(item._id)}><TrashIcon /></button></div>
             </li>)
