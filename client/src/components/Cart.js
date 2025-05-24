f23ce760c150a1eb3b1cbe91250068ab50259a6c
Kevin <kevinlane3@hotmail.com>
2025-05-24 19:17:54 +0200

    cleanup button

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -113,10 +113,10 @@ function Cart() {
               <div id='content-wrapper'><span id='cart-item-amount'>{nameCounts[item.name]}x </span> </div>
               <div id='content-wrapper'><span id='cart-item-name'>{item.name}</span> </div>
               <div id='button-wrapper'><span id='cart-item-price'>{nameCounts[item.name] * item.price}kr</span>
-              <button id='remove-btn' onClick={() => deleteItem(item._id)}><TrashIcon /></button>
-              <button id='remove-btn' onClick={() => addItem(item)}>+</button></div>
-              <button id='remove-btn' onClick={() => deleteItem(item._id)}>-</button>
-
+                <button id='remove-btn' onClick={() => deleteItem(item._id)}><TrashIcon /></button>
+                <button id='remove-btn' onClick={() => deleteItem(item._id)}>-</button>
+                <button id='remove-btn' onClick={() => addItem(item)}>+</button>
+              </div>
             </li>)
           })}
         </ul>
