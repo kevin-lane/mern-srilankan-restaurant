283041c3952aaa3d05e39154c088b18e2bea8a61
Kevin <kevinlane3@hotmail.com>
2025-05-25 14:17:34 +0200

    log cartcount in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import XIcon from './components/icons/xIcon';
 function App() {
   const [cartOpen, setCartOpen] = useState(false);
   const [cartCount, setCartCount] = useState(JSON.parse(localStorage.getItem("cart")  || "[]").length);
+  console.log("Cartcount: " + cartCount);
 
   //To count the amount of items in Cart and display on cart button
   //Add new URL
