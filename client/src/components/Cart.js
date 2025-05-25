c90e597e5201ca89508b0016c79d4c9f6361c9f0
Kevin <kevinlane3@hotmail.com>
2025-05-25 18:20:51 +0200

    Remove trash button

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -133,7 +133,6 @@ function Cart( {setCartCount} ) {
               <div id='content-wrapper'><span id='cart-item-amount'>{nameCounts[item.name]}x </span> </div>
               <div id='content-wrapper'><span id='cart-item-name'>{item.name}</span> </div>
               <div id='button-wrapper'><span id='cart-item-price'>{nameCounts[item.name] * item.price}kr</span>
-                <button id='remove-btn' onClick={() => deleteItem(item._id)}><TrashIcon /></button>
                 <button id='remove-btn' onClick={() => deleteItem(item)}>-</button>
                 <button id='remove-btn' onClick={() => addItem(item)}>+</button>
               </div>
