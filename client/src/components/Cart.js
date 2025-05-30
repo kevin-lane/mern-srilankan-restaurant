ad205f59427a0be09f81fe52cb1354858cfd551f
Kevin <kevinlane3@hotmail.com>
2025-05-30 21:17:08 +0200

    Made cartIconX to a tag element component

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -88,7 +88,7 @@ function Cart( {setCartCount} ) {
 
   return (
     <div id='cart'>
-      {cartItems.length === 0 ? <><h2>{cartText}</h2><p>{cartIconX}</p></> :
+      {cartItems.length === 0 ? <><h2>{cartText}</h2><p><cartIconX /></p></> :
       <>
         <h2>Your cart items: </h2>
         <ul id='cart-list'>
