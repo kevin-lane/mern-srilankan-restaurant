e60fd1aca74b511e72a59611a8317c873f676956
Kevin <kevinlane3@hotmail.com>
2025-05-30 15:55:15 +0200

    texts in separate paragraph

--- client/src/components/modals/OrderConfirmation.js
+++ client/src/components/modals/OrderConfirmation.js
@@ -4,7 +4,8 @@ import CheckIcon from '../icons/checkIcon';
 export default function() {
   return (
     <div id='order-confirmation'>
-      <p>Thank you for your order. Please come to the restaurant and collect your order!</p>
+      <p>Thank you for your order.</p>
+      <p>Please come to the restaurant and collect your order!</p>
       <p><CheckIcon /></p>
       <button id='order-confirmation-button' onClick={() => window.location.reload()}>OK</button>
     </div>
